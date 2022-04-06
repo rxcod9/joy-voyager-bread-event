fe2298246a72356a57aa96da6c4658601cbca11e
Ramakant <gangwar.ramakant@gmail.com>
2022-04-06 09:46:46 +0530

    Icon

--- database/seeders/MenuItemsTableSeeder.php
+++ database/seeders/MenuItemsTableSeeder.php
@@ -28,7 +28,7 @@ class MenuItemsTableSeeder extends Seeder
         if (!$menuItem->exists) {
             $menuItem->fill([
                 'target'     => '_self',
-                'icon_class' => 'voyager-bread voyager-bread-event voyager-calender',
+                'icon_class' => 'voyager-bread voyager-bread-event voyager-calendar',
                 'color'      => null,
                 'parent_id'  => $parentMenuId,
                 'order'      => ++$maxOrder,
